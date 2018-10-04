9cf60ac2e0f9b5be9ee3fd3b6def117501a941bb
kstrotz <kevin.j.strotz@gmail.com>
2018-10-03 23:59:41 -0400

    Initial commit of main file.

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Main function for Sudoku Solver.
+ * 
+ * K. Strotz
+ * 4 Oct 2018
+ */
+
+#include "solver.h"
+
+int main(int argc, char **argv){
+	if (argc > 1){
+		fprintf(stderr, "Usage: %s\n", argv[0]);
+		return 1;
+	}
+	struct grid *Grid = nGrid();
+}
