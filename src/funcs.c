74ea2bba0941b05e2a891b067bbf69137f2559d7
kstrotz <kevin.j.strotz@gmail.com>
2018-10-11 20:24:35 -0400

    Added testcrow.c and updated fgridtst

--- src/funcs.c
+++ src/funcs.c
@@ -71,11 +71,87 @@ void fgrid(struct grid* Grid){
 
 void fgridtst(struct grid* Grid){
 	printf("Filling the grid...\n");
-	for (int i = 0; i < ROWS; i++){
-		for (int j = 0; j < COLUMNS; j++){
-			Grid->values[i][j] = (i+1);
-		}
-	}
+	Grid->values[0][0] = 0;
+	Grid->values[0][1] = 0;
+	Grid->values[0][2] = 0;
+	Grid->values[0][3] = 0;
+	Grid->values[0][4] = 1;
+	Grid->values[0][5] = 2;
+	Grid->values[0][6] = 0;
+	Grid->values[0][7] = 0;
+	Grid->values[0][8] = 6;
+	Grid->values[1][0] = 1;
+	Grid->values[1][1] = 0;
+	Grid->values[1][2] = 8;
+	Grid->values[1][3] = 3;
+	Grid->values[1][4] = 4;
+	Grid->values[1][5] = 0;
+	Grid->values[1][6] = 9;
+	Grid->values[1][7] = 0;
+	Grid->values[1][8] = 0;
+	Grid->values[2][0] = 3;
+	Grid->values[2][1] = 2;
+	Grid->values[2][2] = 4;
+	Grid->values[2][3] = 0;
+	Grid->values[2][4] = 6;
+	Grid->values[2][5] = 0;
+	Grid->values[2][6] = 0;
+	Grid->values[2][7] = 0;
+	Grid->values[2][8] = 0;
+	Grid->values[3][0] = 0;
+	Grid->values[3][1] = 0;
+	Grid->values[3][2] = 2;
+	Grid->values[3][3] = 9;
+	Grid->values[3][4] = 0;
+	Grid->values[3][5] = 0;
+	Grid->values[3][6] = 0;
+	Grid->values[3][7] = 0;
+	Grid->values[3][8] = 0;
+	Grid->values[4][0] = 5;
+	Grid->values[4][1] = 3;
+	Grid->values[4][2] = 9;
+	Grid->values[4][3] = 6;
+	Grid->values[4][4] = 0;
+	Grid->values[4][5] = 4;
+	Grid->values[4][6] = 2;
+	Grid->values[4][7] = 1;
+	Grid->values[4][8] = 7;
+	Grid->values[5][0] = 0;
+	Grid->values[5][1] = 0;
+	Grid->values[5][2] = 0;
+	Grid->values[5][3] = 0;
+	Grid->values[5][4] = 0;
+	Grid->values[5][5] = 1;
+	Grid->values[5][6] = 8;
+	Grid->values[5][7] = 0;
+	Grid->values[5][8] = 0;
+	Grid->values[6][0] = 0;
+	Grid->values[6][1] = 0;
+	Grid->values[6][2] = 0;
+	Grid->values[6][3] = 0;
+	Grid->values[6][4] = 3;
+	Grid->values[6][5] = 0;
+	Grid->values[6][6] = 4;
+	Grid->values[6][7] = 8;
+	Grid->values[6][8] = 5;
+	Grid->values[7][0] = 0;
+	Grid->values[7][1] = 0;
+	Grid->values[7][2] = 1;
+	Grid->values[7][3] = 0;
+	Grid->values[7][4] = 5;
+	Grid->values[7][5] = 7;
+	Grid->values[7][6] = 6;
+	Grid->values[7][7] = 0;
+	Grid->values[7][8] = 9;
+	Grid->values[8][0] = 6;
+	Grid->values[8][1] = 0;
+	Grid->values[8][2] = 0;
+	Grid->values[8][3] = 4;
+	Grid->values[8][4] = 9;
+	Grid->values[8][5] = 0;
+	Grid->values[8][6] = 0;
+	Grid->values[8][7] = 0;
+	Grid->values[8][8] = 0;
 }
 
 void ftgrid(const struct grid* Grid, struct tgrid* tGrid){
---
+++ test/testcrow.c
@@ -0,0 +1,13 @@
+/*
+ * Test program for the crow(...) function.
+ *
+ * K. Strotz
+ * 11 Oct 2018
+ */
+
+#include "../src/solver.h"
+
+int main(int argc, char** argv){
+	struct grid* Grid = ngrid();
+		
+}
